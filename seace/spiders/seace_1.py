2b007f9b209b7bf44c261d102b0873f4a552a91d
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-01-04 22:20:39 -0500

    Add working selenium code

--- seace/spiders/seace_1.py
+++ seace/spiders/seace_1.py
@@ -1,10 +1,14 @@
 import scrapy
+from scrapy_selenium import SeleniumRequest
 
 
 class Seace1Spider(scrapy.Spider):
     name = 'seace_1'
-    allowed_domains = ['https://prodapp2.seace.gob.pe/seacebus-uiwd-pub/buscadorPublico/buscadorPublico.xhtml']
-    start_urls = ['http://https://prodapp2.seace.gob.pe/seacebus-uiwd-pub/buscadorPublico/buscadorPublico.xhtml/']
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        url = 'https://prodapp2.seace.gob.pe/seacebus-uiwd-pub/buscadorPublico/buscadorPublico.xhtml'
+        yield SeleniumRequest(url=url, callback=self.parse)
+
+    def parse(self, response: SeleniumRequest):
+        driver = response.request.meta['driver']
+        driver.save_screenshot('test.png')
