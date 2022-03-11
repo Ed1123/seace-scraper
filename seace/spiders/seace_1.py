ed1acca0aeca8524fe9adb25d4e81b89b989170c
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-03-10 21:47:46 -0500

    Get ExtraData

--- seace/spiders/seace_1.py
+++ seace/spiders/seace_1.py
@@ -2,6 +2,7 @@ import os
 from dataclasses import dataclass
 from datetime import datetime
 from time import sleep
+from typing import Optional
 
 import pandas as pd
 import scrapy
@@ -39,16 +40,16 @@ class ExtraData:
     fecha_presentación_de_ofertas: datetime
     fecha_evaluación_y_calificación: datetime
     fecha_otorgamiento_de_la_buena_pro: datetime
-    postores: list[str]
-    consorcio: str
-    fecha_de_cambio_de_estado: datetime
-    estado_anterior: str
-    # cambio_de
-    # cambio_a
-    fecha_de_cambio_de_postor: datetime
-    postor_anterior: str
-    # cambio_de_postor_de
-    # cambio_de_postor_a
+    postores: list[Optional[str]]
+    consorcios: list[Optional[str]]
+    # fecha_de_cambio_de_estado: datetime
+    # estado_anterior: str
+    # # cambio_de
+    # # cambio_a
+    # fecha_de_cambio_de_postor: datetime
+    # postor_anterior: str
+    # # cambio_de_postor_de
+    # # cambio_de_postor_a
 
 
 class Seace1Spider(scrapy.Spider):
@@ -166,7 +167,7 @@ class Seace1Spider(scrapy.Spider):
                 )
 
                 # Checking for the "bad captcha" message box
-                message_box = WebDriverWait(self.driver, 1).until(
+                message_box = WebDriverWait(self.driver, 5).until(
                     EC.visibility_of_element_located(
                         (By.XPATH, '//*[@id="frmMesajes:gPrincipal_container"]/div')
                     )
@@ -191,6 +192,7 @@ class Seace1Spider(scrapy.Spider):
             except TimeoutException:
                 # Else, the catpcha is solved and just break the infinite loop
                 break
+        sleep(1)
 
     def get_cui(self) -> str:
         self.click_element(
@@ -214,9 +216,44 @@ class Seace1Spider(scrapy.Spider):
             f'//*[@id="tbBuscador:idFormBuscarProceso:j_idt41_panel"]/div/ul/li[@data-label="{objeto_contratación}"]'
         )
 
+    def get_text(self, xpath: str) -> str:
+        return self.driver.find_element_by_xpath(xpath).text
+
+    def get_datetime(self, xpath: str) -> datetime:
+        date_text = self.driver.find_element_by_xpath(xpath).text
+        if len(date_text) == 16:
+            return datetime.strptime(date_text, '%d/%m/%Y %H:%M')
+        elif len(date_text) == 10:
+            return datetime.strptime(date_text, '%d/%m/%Y')
+        else:
+            raise SyntaxError(
+                f'{date_text} is not in the correct format. Either dd/mm/yyyy hh:mm or dd/mm/yyyy.'
+            )
+
+    def get_postores_consorcios(
+        self,
+    ) -> tuple[list[Optional[str]], list[Optional[str]]]:
+        postores = []
+        consorcios = []
+        for element in self.driver.find_elements_by_xpath(
+            '//*[@id="tbFicha:idGridLstItems:0:dtParticipantes_data"]/tr/td[1]'
+        ):
+            if element.text[:3].isdigit():
+                postores.append(element.text)
+            else:
+                consorcios.append(element.text)
+        return postores, consorcios
+
     def get_extra_data(
         self, descripción_objeto: str, date: datetime, objeto_contratación: str
     ) -> ExtraData:
+        '''
+        Testing code:
+        descripción_objeto = 'ADQUISICION DE SEMILLA DE AVENA FORRAJERA (AVENA SATIVA L.) VARIEDAD MANTARO 15 , CLASE NO CERTIFICADA PARA DZ - AYACUCHO'
+        date = datetime.strptime('03/11/2021 23:48', '%d/%m/%Y %H:%M')
+        objeto_contratación = 'Bien'
+        self.get_extra_data(descripción_objeto, date, objeto_contratación)
+        '''
         # Input data
         self.fill_box(
             '//*[@id="tbBuscador:idFormBuscarProceso:descripcionObjeto"]',
@@ -225,12 +262,52 @@ class Seace1Spider(scrapy.Spider):
         self.select_year(date.year)
         self.select_objeto_contratación(objeto_contratación)
         # May be needed to input the full date and compare the full timpestamp after searching, just for multiple results.
+
         self.fill_catpcha_and_search()
+        # Sleep while waiting for the search to load.
+        # Include in the fill_catpcha_and_search method during the optimization
+        sleep(10)
 
+        # Get CUI before entering the calendar view
         cui = self.get_cui()
-        return ExtraData()
 
+        # Click calendar
+        self.click_element(
+            '//*[@id="tbBuscador:idFormBuscarProceso:dtProcesos:0:grafichaSel"]'
+        )
 
-descripción_objeto = 'ADQUISICION DE SEMILLA DE AVENA FORRAJERA (AVENA SATIVA L.) VARIEDAD MANTARO 15 , CLASE NO CERTIFICADA PARA DZ - AYACUCHO'
-date = datetime.strptime('03/11/2021 23:48', '%d/%m/%Y %H:%M')
-objeto_contratación = 'Bien'
+        # Click "Ver listado de item"
+        self.click_element('//*[@id="tbFicha:j_idt1204"]/legend')
+        sleep(1)
+
+        postores, consorcios = self.get_postores_consorcios()
+
+        return ExtraData(
+            cui=cui,
+            estado=self.get_text(
+                '//*[@id="tbFicha:idGridLstItems_content"]/table/tbody/tr[1]/td/table[2]/tbody/tr[2]/td[8]'
+            ),
+            fecha_registro_de_participantes=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[2]/td[3]'
+            ),
+            fecha_formulación_de_consultas_y_observaciones=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[3]/td[3]'
+            ),
+            fecha_absolución_de_consultas_y_observaciones=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[4]/td[3]'
+            ),
+            fecha_integración_de_las_bases=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[5]/td[3]'
+            ),
+            fecha_presentación_de_ofertas=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[6]/td[3]'
+            ),
+            fecha_evaluación_y_calificación=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[7]/td[3]'
+            ),
+            fecha_otorgamiento_de_la_buena_pro=self.get_datetime(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr[8]/td[3]'
+            ),
+            postores=postores,
+            consorcios=consorcios,
+        )
