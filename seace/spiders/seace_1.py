9f606708b49ed54d5853f4dd05c6e4aa8ed02c78
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-03-18 17:47:17 -0500

    Modify how the cronograma is get
    Also ignore wrong type checking and fix bug with row generator and objeto
    de contratación. Also add to_datetime base method

--- seace/spiders/seace_1.py
+++ seace/spiders/seace_1.py
@@ -1,16 +1,16 @@
 import os
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 from datetime import datetime
 from time import sleep
 from typing import Optional
 
 import pandas as pd
 import scrapy
-from attr import asdict
 from scrapy.utils.conf import closest_scrapy_cfg
 from selenium import webdriver
 from selenium.common.exceptions import (
     ElementNotInteractableException,
+    NoSuchElementException,
     StaleElementReferenceException,
     TimeoutException,
 )
@@ -35,13 +35,14 @@ def solve_captcha(captcha: bytes) -> str:
 class ExtraData:
     cui: str
     estado: str
-    fecha_registro_de_participantes: datetime
-    fecha_formulación_de_consultas_y_observaciones: datetime
-    fecha_absolución_de_consultas_y_observaciones: datetime
-    fecha_integración_de_las_bases: datetime
-    fecha_presentación_de_ofertas: datetime
-    fecha_evaluación_y_calificación: datetime
-    fecha_otorgamiento_de_la_buena_pro: datetime
+    cronograma: dict[str, datetime]
+    # fecha_registro_de_participantes: Optional[datetime]
+    # fecha_formulación_de_consultas_y_observaciones: Optional[datetime]
+    # fecha_absolución_de_consultas_y_observaciones: Optional[datetime]
+    # fecha_integración_de_las_bases: Optional[datetime]
+    # fecha_presentación_de_ofertas: Optional[datetime]
+    # fecha_evaluación_y_calificación: Optional[datetime]
+    # fecha_otorgamiento_de_la_buena_pro: Optional[datetime]
     postores: list[Optional[str]]
     consorcios: list[Optional[str]]
     # fecha_de_cambio_de_estado: datetime
@@ -62,10 +63,10 @@ class SeaceScraper:
         # Waiting for the grey box window to appear and dissipate
         # Max of 10 minutes each
         grey_box = '//div[contains(@id, "blocker")]'
-        WebDriverWait(self.driver, 600).until(
+        WebDriverWait(self.driver, 600).until(  # type: ignore
             EC.visibility_of_element_located((By.XPATH, grey_box))
         )
-        WebDriverWait(self.driver, 600).until(
+        WebDriverWait(self.driver, 600).until(  # type: ignore
             EC.invisibility_of_element((By.XPATH, grey_box))
         )
 
@@ -86,17 +87,17 @@ class SeaceScraper:
                 # (after the grey box dissipated), the captcha wasn't solved and the loop
                 # will continue, else, it will raise a TimeoutException because the message
                 # box didn't appear indicating the captcha was solved.
-                message_box = WebDriverWait(self.driver, 5).until(
+                message_box = WebDriverWait(self.driver, 5).until(  # type: ignore
                     EC.visibility_of_element_located(
                         (By.XPATH, '//*[@id="frmMesajes:gPrincipal_container"]/div')
                     )
                 )
                 # Close message box for next try
-                ActionChains(self.driver).move_to_element(
+                ActionChains(self.driver).move_to_element(  # type: ignore
                     message_box
                 ).perform()  # Hover to make the 'x' visible
                 try:
-                    WebDriverWait(self.driver, 7).until(
+                    WebDriverWait(self.driver, 7).until(  # type: ignore
                         EC.visibility_of_element_located(
                             (
                                 By.XPATH,
@@ -115,10 +116,10 @@ class SeaceScraper:
         sleep(1)
 
     def get_text(self, xpath: str) -> str:
-        return self.driver.find_element_by_xpath(xpath).text
+        return self.driver.find_element_by_xpath(xpath).text  # type: ignore
 
     def get_captcha(self) -> str:
-        captcha_img = self.driver.find_element_by_xpath(
+        captcha_img = self.driver.find_element_by_xpath(  # type: ignore
             '//*[@id="tbBuscador:idFormBuscarProceso:captchaImg"]'
         ).screenshot_as_png
         return captcha_img
@@ -137,19 +138,19 @@ class SeaceScraper:
         while True:
             try:
                 # box = wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
-                box = self.driver.find_element_by_xpath(xpath)
+                box = self.driver.find_element_by_xpath(xpath)  # type: ignore
                 box.click()
                 # Clear field before entering the input string
                 box.clear()
                 box.send_keys(input_str)
                 break
             except (StaleElementReferenceException, ElementNotInteractableException):
-                self.logger.debug(f'Trying to input date again. Retry: {i + 1}')
+                self.logger.debug(f'Trying to input date again. Retry: {i + 1}')  # type: ignore
                 sleep(0.5)
                 i += 1
 
     def click_element(self, xpath: str) -> None:
-        self.driver.find_element_by_xpath(xpath).click()
+        self.driver.find_element_by_xpath(xpath).click()  # type: ignore
 
     def select_year(self, year: int) -> None:
         '''Clicks the dropdown and select the year'''
@@ -160,18 +161,28 @@ class SeaceScraper:
             f'//*[@id="tbBuscador:idFormBuscarProceso:anioConvocatoria_panel"]/div/ul/li[@data-label={year}]'
         )
 
+    def to_datetime(self, date_text: str) -> datetime:
+        if len(date_text) == 16:
+            return datetime.strptime(date_text, '%d/%m/%Y %H:%M')
+        elif len(date_text) == 10:
+            return datetime.strptime(date_text, '%d/%m/%Y')
+        else:
+            raise SyntaxError(
+                f'{date_text} is not in the correct format. Either dd/mm/yyyy hh:mm or dd/mm/yyyy.'
+            )
+
 
 class Seace1Spider(scrapy.Spider, SeaceScraper):
     name = 'seace_1'
 
     def start_requests(self):
-        options = webdriver.ChromeOptions()
+        options = webdriver.ChromeOptions()  # type: ignore
         if not os.getenv('TEST_MODE'):
             options.add_argument('headless')
         download_path = os.path.join(os.path.dirname(closest_scrapy_cfg()), 'output')
         prefs = {'download.default_directory': download_path}
         options.add_experimental_option('prefs', prefs)
-        self.driver = webdriver.Chrome(
+        self.driver = webdriver.Chrome(  # type: ignore
             executable_path=ChromeDriverManager().install(), chrome_options=options
         )
         self.driver.get(
@@ -180,9 +191,6 @@ class Seace1Spider(scrapy.Spider, SeaceScraper):
         yield scrapy.Request(url='http://quotes.toscrape.com')
 
     def parse(self, response):
-        def get_column_in_row(id: int):
-            return row.find_element_by_xpath(f'./td[{id}]').text
-
         extra_data_extractor = ExtraDataExtractor(self.driver)
 
         # Click "Búsqueda avanzada"
@@ -193,19 +201,17 @@ class Seace1Spider(scrapy.Spider, SeaceScraper):
             rows = self.get_data_for_a_date(date)
             for row in rows:
                 data = {
-                    'Nombre o Sigla de la Entidad': get_column_in_row(2),
-                    'Fecha y Hora de Publicacion': get_column_in_row(3),
-                    'Nomenclatura': get_column_in_row(4),
-                    'Objeto de Contratación': get_column_in_row(6),
-                    'Descripción de Objeto': get_column_in_row(7),
-                    'Valor Referencial / Valor Estimado': get_column_in_row(10),
-                    'Moneda': get_column_in_row(11),
+                    'Nombre o Sigla de la Entidad': row[1],
+                    'Fecha y Hora de Publicacion': row[2],
+                    'Nomenclatura': row[3],
+                    'Objeto de Contratación': row[5],
+                    'Descripción de Objeto': row[6],
+                    'Valor Referencial / Valor Estimado': row[9],
+                    'Moneda': row[10],
                 }
                 extra_data = extra_data_extractor.get_extra_data(
                     data['Descripción de Objeto'],
-                    datetime.strptime(
-                        data['Fecha y Hora de Publicacion'], '%d/%m/%Y %H:%M'
-                    ),
+                    self.to_datetime(data['Fecha y Hora de Publicacion']),
                     data['Objeto de Contratación'],
                 )
                 yield data | asdict(extra_data)
@@ -215,13 +221,14 @@ class Seace1Spider(scrapy.Spider, SeaceScraper):
 
     def get_date_range_parameter(self) -> pd.DatetimeIndex:
         try:
-            return pd.date_range(start=self.start_date, end=self.end_date)
+            return pd.date_range(start=self.start_date, end=self.end_date)  # type: ignore
         except AttributeError:
             raise AttributeError('Must include parameters start_date and end_dated.')
         except ValueError:
             raise ValueError('Must provide date in the format \'YYYY-MM-DD\'.')
 
-    def get_data_for_a_date(self, date: datetime):
+    def get_data_for_a_date(self, date: datetime) -> list[list[str]]:
+        '''Returns a list of rows (another list) with the data as a str.'''
         # Enter dates
         self.fill_date(date)
 
@@ -229,10 +236,12 @@ class Seace1Spider(scrapy.Spider, SeaceScraper):
 
         # Click export to download the file
         sleep(1)
-        return self.driver.find_elements_by_xpath(
-            '//*[@id="tbBuscador:idFormBuscarProceso:dtProcesos_data"]/tr'
-        )
-        self.click_element('//*[@id="tbBuscador:idFormBuscarProceso:btnExportar"]')
+        return [
+            [row.find_element_by_xpath(f'./td[{i}]').text for i in range(1, 13)]
+            for row in self.driver.find_elements_by_xpath(
+                '//*[@id="tbBuscador:idFormBuscarProceso:dtProcesos_data"]/tr'
+            )
+        ]
 
     def fill_date(self, date: datetime) -> None:
         self.select_year(date.year)
@@ -269,19 +278,13 @@ class ExtraDataExtractor(SeaceScraper):
         self.click_element(
             '/html/body/div[3]/div/div[1]/div[1]/div[1]/form/table[1]/tbody/tr[1]/td/table/tbody/tr[2]/td[2]/div/label'
         )
-        self.click_element(
-            f'/html/body/div[15]/div/ul/li[@data-label="{objeto_contratación}"]'
-        )
-
-    def get_datetime(self, xpath: str) -> datetime:
-        date_text = self.driver.find_element_by_xpath(xpath).text
-        if len(date_text) == 16:
-            return datetime.strptime(date_text, '%d/%m/%Y %H:%M')
-        elif len(date_text) == 10:
-            return datetime.strptime(date_text, '%d/%m/%Y')
-        else:
-            raise SyntaxError(
-                f'{date_text} is not in the correct format. Either dd/mm/yyyy hh:mm or dd/mm/yyyy.'
+        try:
+            self.click_element(
+                f'/html/body/div[7]/div/ul/li[@data-label="{objeto_contratación}"]'
+            )
+        except NoSuchElementException:
+            self.click_element(
+                f'/html/body/div[15]/div/ul/li[@data-label="{objeto_contratación}"]'
             )
 
     def get_postores_consorcios(
@@ -297,6 +300,16 @@ class ExtraDataExtractor(SeaceScraper):
             else:
                 consorcios.append(element.text)
         return postores, consorcios
+
+    def get_cronograma(self) -> dict[str, datetime]:
+        return {
+            element.find_element_by_xpath('./td[1]').text: self.to_datetime(
+                element.find_element_by_xpath('./td[3]').text
+            )
+            for element in self.driver.find_elements_by_xpath(
+                '//*[@id="tbFicha:dtCronograma_data"]/tr'
+            )
+        }
 
     def get_extra_data(
         self, descripción_objeto: str, date: datetime, objeto_contratación: str
@@ -337,32 +350,14 @@ class ExtraDataExtractor(SeaceScraper):
 
         postores, consorcios = self.get_postores_consorcios()
 
+        cronograma = self.get_cronograma()
+
         extra_data = ExtraData(
             cui=cui,
             estado=self.get_text(
                 '//*[@id="tbFicha:idGridLstItems_content"]/table/tbody/tr[1]/td/table[2]/tbody/tr[2]/td[8]'
             ),
-            fecha_registro_de_participantes=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[2]/td[3]'
-            ),
-            fecha_formulación_de_consultas_y_observaciones=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[3]/td[3]'
-            ),
-            fecha_absolución_de_consultas_y_observaciones=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[4]/td[3]'
-            ),
-            fecha_integración_de_las_bases=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[5]/td[3]'
-            ),
-            fecha_presentación_de_ofertas=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[6]/td[3]'
-            ),
-            fecha_evaluación_y_calificación=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[7]/td[3]'
-            ),
-            fecha_otorgamiento_de_la_buena_pro=self.get_datetime(
-                '//*[@id="tbFicha:dtCronograma_data"]/tr[8]/td[3]'
-            ),
+            cronograma=cronograma,
             postores=postores,
             consorcios=consorcios,
         )
