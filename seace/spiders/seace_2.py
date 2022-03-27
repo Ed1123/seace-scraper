956d3a6763601bcd59a7103994826d40fef7bd37
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-03-27 18:24:19 -0500

    Add code for working seace_2

--- seace/spiders/seace_2.py
+++ seace/spiders/seace_2.py
@@ -1,10 +1,21 @@
-import scrapy
+from datetime import datetime
+from time import sleep
+
+from .seace_1 import ExtraDataExtractor, Seace1Spider
 
 
-class Seace2Spider(scrapy.Spider):
+class Seace2Spider(Seace1Spider):
     name = 'seace_2'
-    allowed_domains = ['http://quotes.toscrape.com']
-    start_urls = ['http://http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        extra_data_extractor = ExtraDataExtractor(self.driver)
+
+        extra_data = extra_data_extractor.get_extra_data(
+            'ADQUISICION DE SEMILLA DE AVENA FORRAJERA (AVENA SATIVA L.) VARIEDAD MANTARO 15 , CLASE NO CERTIFICADA PARA DZ - AYACUCHO',
+            datetime.strptime('03/11/2021 23:48', '%d/%m/%Y %H:%M'),
+            'Bien',
+        )
+        yield extra_data
+
+        # Close browser after finishing the scrapping
+        self.driver.close()
