c0bfb35318ab23aa62f3d0629ebb469f22c55cfb
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2022-03-27 19:19:32 -0500

    Get parameters from csv file

--- seace/spiders/seace_2.py
+++ seace/spiders/seace_2.py
@@ -1,5 +1,6 @@
+import csv
 from datetime import datetime
-from time import sleep
+from typing import Iterable
 
 from .seace_1 import ExtraDataExtractor, Seace1Spider
 
@@ -7,15 +8,31 @@ from .seace_1 import ExtraDataExtractor, Seace1Spider
 class Seace2Spider(Seace1Spider):
     name = 'seace_2'
 
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, **kwargs)
+        try:
+            self.filepath_parameter = self.filepath  # type: ignore
+        except AttributeError:
+            raise AttributeError(
+                'Must include a filepath parameter (eg. "-a filepath=here/paramenters.csv").'
+            )
+
+    def read_parameters(self) -> Iterable[tuple[str, datetime, str]]:
+        with open(self.filepath_parameter, newline='') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                yield (
+                    row[0],
+                    datetime.strptime(row[1], '%d/%m/%Y %H:%M'),
+                    row[2],
+                )
+
     def parse(self, response):
         extra_data_extractor = ExtraDataExtractor(self.driver)
 
-        extra_data = extra_data_extractor.get_extra_data(
-            'ADQUISICION DE SEMILLA DE AVENA FORRAJERA (AVENA SATIVA L.) VARIEDAD MANTARO 15 , CLASE NO CERTIFICADA PARA DZ - AYACUCHO',
-            datetime.strptime('03/11/2021 23:48', '%d/%m/%Y %H:%M'),
-            'Bien',
-        )
-        yield extra_data
+        for parameters in self.read_parameters():
+            extra_data = extra_data_extractor.get_extra_data(*parameters)
+            yield extra_data
 
         # Close browser after finishing the scrapping
         self.driver.close()
